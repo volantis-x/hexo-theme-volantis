f92fd0fda2eb63e35eb478ae0debeddcdd65e6df
honjun <hj531@live.com>
2018-11-25 19:12:03 +0800

    增加pjax支持

--- source/js/app.js
+++ source/js/app.js
@@ -1,4 +1,10 @@
 /* eslint-disable */
+$(document).pjax('a', '#pjax-container', {
+    fragment: '#pjax-container',
+    timeout: 5000,
+    cache: false
+});
+
 var customSearch;
 (function ($) {
 
