9d19ea78d10480c75dccb66a53c2a27ca4b05a5f
CRainyDay <46487957+CRainyDay@users.noreply.github.com>
2019-03-23 21:58:36 +0800

    copy code
    copy code

---
+++ source/js/clipboard-use.js
@@ -0,0 +1,17 @@
+/*页面载入完成后，创建复制按钮*/
+!function (e, t, a) { 
+  /* code */
+  var initCopyCode = function(){
+    var copyHtml = '';
+    copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
+    copyHtml += '  <i class="fa fa-globe"></i><span>copy</span>';
+    copyHtml += '</button>';
+    $(".highlight .code pre").before(copyHtml);
+    new ClipboardJS('.btn-copy', {
+        target: function(trigger) {
+            return trigger.nextElementSibling;
+        }
+    });
+  }
+  initCopyCode();
+}(window, document);
