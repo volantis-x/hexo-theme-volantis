53304cf84f0aff3b9ac6c5cf7752c3cdaf947508
xaoxuu <xaoxuu@gmail.com>
2020-03-28 19:15:43 +0800

    span & note

--- scripts/tags/note.js
+++ scripts/tags/note.js
@@ -5,11 +5,18 @@
 
 'use strict';
 
-function postNote(args, content) {
+function postNote(args) {
+  args = args.join(' ').split(',')
+  let p0 = args[0].trim()
+  let p1 = args[1].trim()
+  return `<div class="note ${p0}">${hexo.render.renderSync({text: p1, engine: 'markdown'}).split('\n').join('')}</div>`;
+}
+
+function postNoteBlock(args, content) {
   return `<div class="note ${args.join(' ')}">
             ${hexo.render.renderSync({text: content, engine: 'markdown'}).split('\n').join('')}
           </div>`;
 }
 
-hexo.extend.tag.register('note', postNote, {ends: true});
-hexo.extend.tag.register('subnote', postNote, {ends: true});
+hexo.extend.tag.register('note', postNote);
+hexo.extend.tag.register('noteblock', postNoteBlock, {ends: true});
