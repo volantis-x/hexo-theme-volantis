b92b3dd96c2843b1c493398ac2b2cbf2d2988d86
xaoxuu <xaoxuu@gmail.com>
2020-03-28 22:02:54 +0800

    css

--- scripts/tags/folding.js
+++ scripts/tags/folding.js
@@ -2,8 +2,14 @@
 
 function postFolding(args, content) {
   args = args.join(' ').split(',');
-  var title = args[0];
-  var style = args[1];
+  let style = ''
+  let title = ''
+  if (args.length > 1) {
+    style = args[0].trim()
+    title = args[1].trim()
+  } else if (args.length > 0) {
+    title = args[0].trim()
+  }
   if (style != undefined) {
     return `<details ${style}><summary> ${title} </summary>
               <div class='content'>
