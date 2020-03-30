0a9c09d709ea0328d12b8e0a7dbb306216bbfa4b
xaoxuu <xaoxuu@gmail.com>
2020-03-30 16:39:42 +0800

    update

--- scripts/tags/video.js
+++ scripts/tags/video.js
@@ -2,7 +2,22 @@
 
 function postVideo(args) {
   let src = args[0].trim()
-  return `<video controls preload width='100%' height='100%'><source src='${src}' type='video/mp4'>Your browser does not support the video tag.</video>`;
+  return `<video controls preload><source src='${src}' type='video/mp4'>Your browser does not support the video tag.</video>`;
+}
+
+function postVideos(args, content) {
+  args = args.join(' ').split(',')
+  var cls = args[0]
+  if (cls.length > 0) {
+    cls = ' ' + cls
+  }
+  var col = Number(args[1]) || 0;
+  if (col > 0) {
+    return `<div class="videos${cls}" col='${col}'>${content}</div>`
+  } else {
+    return `<div class="videos${cls}">${content}</div>`
+  }
 }
 
 hexo.extend.tag.register('video', postVideo);
+hexo.extend.tag.register('videos', postVideos, {ends: true});
