8e2b2e3a068ef7e1cfb87d4fc1e1a9bb4cd46773
MHuiG <616001163@qq.com>
2020-05-02 16:22:41 +0800

    up

--- source/js/app.js
+++ source/js/app.js
@@ -352,3 +352,22 @@ var customSearch;
 	});
 
 })(jQuery);
+
+/*code-block-fullscreen*/
+$("figcaption").click(function () {
+	if (window.CodeBlockFullscreen) {
+		$("#post").removeClass("code-block-fullscreen");
+		$(this).parent().removeClass("code-block-fullscreen");
+		$(this).parent().removeClass("code-block-fullscreen-overflow-auto");
+		$("html").removeClass("code-block-fullscreen-html-scroll");
+		$(".highlight>table .gutter").removeClass("code-block-fullscreen-gutter");
+		window.CodeBlockFullscreen = false
+	} else {
+		$("#post").addClass("code-block-fullscreen");
+		$(this).parent().addClass("code-block-fullscreen");
+		$(this).parent().addClass("code-block-fullscreen-overflow-auto");
+		$("html").addClass("code-block-fullscreen-html-scroll");
+		$(".highlight>table .gutter").addClass("code-block-fullscreen-gutter");
+		window.CodeBlockFullscreen = true
+	}
+});
