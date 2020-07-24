d56761ffcf3168a070ba14b7863b8c35acd42d80
jasonhuang <jasonhuang@hyx.com>
2020-07-24 18:14:32 +0800

    关于我页面 && 历史日程修改，jquery.min.js放到header中

--- source/js/timeline.js
+++ source/js/timeline.js
@@ -15,22 +15,22 @@ function systole(){
 	$warpEle.parent().css({"height":59});
 	
 	setTimeout(function(){
-		
-		$warpEle.find("ul").children(":not('h2:first')").each(function(idx){
+		$warpEle.find("ul").children(":not('.first-li')").each(function(idx){
 			eleTop.push($(this).position().top);
 			$(this).css({"margin-top":-eleTop[idx]}).children().hide();
-		}).animate({"margin-top":0}, 1600).children().fadeIn();
+		}).animate({"margin-top":0}, 800).children().fadeIn();
 
 		$warpEle.parent().animate({"height":parentH}, 1500);
 
 		$warpEle.find("ul").children(":not('h2:first')").addClass("bounceInDown").css({"-webkit-animation-duration":"2s","-webkit-animation-delay":"0","-webkit-animation-timing-function":"ease","-webkit-animation-fill-mode":"both"}).end().children("h2").css({"position":"relative"});
 		
-	}, 300);
+	}, 200);
 
 	$targetA.click(function(){
-		$(this).parent().css({"position":"relative"});
-		$(this).parent().siblings().slideToggle();
+		$(this).parent().parent().css({"position":"relative"});		// parent为同级的li元素
+		$(this).parent().parent().siblings().slideToggle();
 		$warpEle.parent().removeAttr("style");
 		return false;
 	});
 };
+window.systole = systole
