7a8f8ea6948b9ec93ed214ad007153be913471fd
xaoxuu <xaoxuu@gmail.com>
2020-07-27 17:29:32 +0800

    inline-labels

---
+++ scripts/tags/inline-labels.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+hexo.extend.tag.register('u', function(args) {
+  return `<u>${args.join(' ')}</u>`;
+});
+hexo.extend.tag.register('emp', function(args) {
+  return `<emp>${args.join(' ')}</emp>`;
+});
+hexo.extend.tag.register('del', function(args) {
+  return `<del>${args.join(' ')}</del>`;
+});
+hexo.extend.tag.register('kbd', function(args) {
+  return `<kbd>${args.join(' ')}</kbd>`;
+});
+hexo.extend.tag.register('psw', function(args) {
+  return `<psw>${args.join(' ')}</psw>`;
+});
