fdb3a4ddbd218181a345f84e6123c1bbdc555d6f
Colsrch <colsrch@foxmail.com>
2020-08-01 00:09:54 +0800

    修复播放暂停不更换图标，上下首无效的问题

--- source/js/aplayer.js
+++ source/js/aplayer.js
@@ -1,29 +1,52 @@
-// 播放/暂停
-function aplayerToggle() {
-	let ap = document.querySelector('meting-js').aplayer;
-	if (ap != undefined) {
-		ap.toggle();
+// 播放
+function aplayerPlay() {
+	var canplay = getCookie('canplay');
+	if (canplay == null || canplay == 'true') {
+		document.querySelector('meting-js').aplayer.play();
+		if (document.getElementById('aplayerPlay').style.display != 'none') {
+			document.getElementById("aplayerPlay").style.display = 'none';
+			document.getElementById("aplayerPause").style.display = '';
+		}
 	}
 }
-
-// 上一曲
+// 暂停
+function aplayerPause() {
+	var canplay = getCookie('canplay');
+	if (canplay == null || canplay == 'true') {
+		document.querySelector('meting-js').aplayer.pause();
+		if (document.getElementById('aplayerPause').style.display != 'none') {
+			document.getElementById("aplayerPause").style.display = 'none';
+			document.getElementById("aplayerPlay").style.display = '';
+		}
+		var index = document.querySelector('meting-js').aplayer.list.index;
+		var title = document.querySelector('meting-js').aplayer.list.audios[index].title;
+		var artist = document.querySelector('meting-js').aplayer.list.audios[index].artist;
+	}
+}
+// 下一首
 function aplayerBackward() {
-	let ap = document.querySelector('meting-js').aplayer;
-	if (ap != undefined) {
-		ap.skipBack();
-		ap.play();
+	var canplay = getCookie('canplay');
+	if (canplay == null || canplay == 'true') {
+		document.querySelector('meting-js').aplayer.skipBack();
+		document.querySelector('meting-js').aplayer.play();
+		if (document.getElementById('aplayerPlay').style.display != 'none') {
+			document.getElementById("aplayerPlay").style.display = 'none';
+			document.getElementById("aplayerPause").style.display = '';
+		}
 	}
 }
-
-// 下一曲
+// 上一首
 function aplayerForward() {
-	let ap = document.querySelector('meting-js').aplayer;
-	if (ap != undefined) {
-		ap.skipForward();
-		ap.play();
+	var canplay = getCookie('canplay');
+	if (canplay == null || canplay == 'true') {
+		document.querySelector('meting-js').aplayer.skipForward();
+		document.querySelector('meting-js').aplayer.play();
+		if (document.getElementById('aplayerPlay').style.display != 'none') {
+			document.getElementById("aplayerPlay").style.display = 'none';
+			document.getElementById("aplayerPause").style.display = '';
+		}
 	}
 }
-
 // 调节音量
 function aplayerVolume(percent) {
 	let ap = document.querySelector('meting-js').aplayer;
