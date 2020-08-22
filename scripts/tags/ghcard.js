a0e0753a8751fab3591d73900f3075a6839c15a1
xaoxuu <xaoxuu@gmail.com>
2020-08-22 23:05:20 +0800

    GitHub 卡片

---
+++ scripts/tags/ghcard.js
@@ -0,0 +1,32 @@
+/**
+ * https://github.com/anuraghazra/github-readme-stats
+ */
+
+'use strict';
+
+// {% ghcard volantis-x %}
+// {% ghcard volantis-x/hexo-theme-volantis %}
+hexo.extend.tag.register('ghcard', function(args) {
+  args = args.join(' ').split(', ');
+  let path = args[0].trim();
+  let card = '';
+  card += '<a class="ghcard" rel="external nofollow noopener noreferrer" href="https://github.com/' + path + '">';
+  let url = '';
+  if (path.includes('/')) {
+    // is repo
+    let ps = path.split('/');
+    url += 'https://github-readme-stats.vercel.app/api/pin/?username=' + ps[0] + "&repo=" + ps[1];
+  } else {
+    // is user
+    url += 'https://github-readme-stats.vercel.app/api/?username=' + path;
+  }
+  if (args.length > 1) {
+    for (let i = 1; i < args.length; i++) {
+      let tmp = args[i].trim();
+      url += "&" + tmp;
+    }
+  }
+  card += '<img src="' + url + '"/>';
+  card += '</a>';
+  return card;
+});
