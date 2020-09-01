53470f011692678183634205b2319b0d2635787d
xaoxuu <xaoxuu@gmail.com>
2020-09-01 16:39:04 +0800

    Revert "添加 fancybox 开关 (#469)"
    This reverts commit ecd2083e4ef6f172b3cfc357bcf4c606743bba72.

--- scripts/fancybox/index.js
+++
@@ -1,4 +0,0 @@
-'use strict'; 
- 
-hexo.extend.filter.register('after_post_render', require('./lib/fancybox').processPost); 
-hexo.extend.filter.register('after_render:html',  require('./lib/fancybox').processSite); 
