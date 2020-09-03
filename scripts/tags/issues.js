1d9d1546a30c9d4f343d50ebc0cfd89498d8ba74
xaoxuu <xaoxuu@gmail.com>
2020-09-03 18:04:22 +0800

    issues api

---
+++ scripts/tags/issues.js
@@ -0,0 +1,42 @@
+/**
+ * issues.js | https://github.com/volantis-x/hexo-theme-volantis
+ */
+
+'use strict';
+
+// 从 issues 加载动态数据
+// {% issues sites/timeline/friends | api=xxx | group=xxx %}
+// 例如：
+// {% issues site | api=https://api.github.com/repos/xaoxuu/friends/issues?sort=updated&state=open&page=1&per_page=100&labels=active | group=recent,developer,student,designer,org,unsafe %}
+hexo.extend.tag.register('issues', function(args) {
+  args = args.join(' ').split(' | ');
+  // 所有支持的参数
+  let type = args[0].trim();
+  let api = '';
+  let group = '';
+  // 解析
+  if (args.length > 1) {
+    for (let i = 1; i < args.length; i++) {
+      let tmp = args[i].trim();
+      if (tmp.includes('type=')) {
+        type = tmp.substring(5, tmp.length);
+      } else if (tmp.includes('api=')) {
+        api = tmp.substring(4, tmp.length);
+      } else if (tmp.includes('group=')) {
+        group = tmp.substring(6, tmp.length);
+      }
+    }
+  }
+  console.log(type, api, group);
+  if (type.length == 0 || api.length == 0) {
+    return;
+  }
+  // 布局
+  let ret = '<div class="issues-api ' + type + '"';
+  ret += 'api="' + api + '"';
+  if (group.length > 0) {
+    ret += 'group="' + group + '"';
+  }
+  ret += '></div>';
+  return ret;
+});
---
+++ source/js/issues.js
@@ -0,0 +1,146 @@
+function parseIssuesData(cfg, data) {
+  let groups = new Object();
+  if (data.length > 0) {
+    if (cfg.group == undefined) {
+      for (i = 0; i < data.length; i++) {
+        let obj = groups['x'];
+        if (obj == undefined) {
+          obj = new Object();
+          obj.name = 'x';
+          obj.items = [data[i]];
+          groups['x'] = obj;
+        } else {
+          obj.items.push(data[i]);
+          groups['x'] = obj;
+        }
+      }
+    } else {
+      for (i = 0; i < data.length; i++) {
+        let issue = data[i];
+        let lbs = issue.labels;
+        if (lbs.length > 0) {
+          for (j = 0; j < lbs.length; j++) {
+            let lb = lbs[j].name;
+            if (cfg.group.includes(lb)) {
+              let obj = groups[lb];
+              if (obj == undefined) {
+                obj = new Object();
+                obj.name = lbs[j].description;
+                obj.items = [issue];
+                groups[lb] = obj;
+              } else {
+                obj.items.push(issue);
+                groups[lb] = obj;
+              }
+            }
+          }
+        }
+      }
+    }
+
+  }
+  return groups;
+}
+
+function getIssuesAPIForSites(cfg) {
+  let el = $(cfg.el)[0];
+  $(el).append('<div class="loading"><i class="fa fa-cog fa-2x fa-spin"></i><p>正在加载</p></div>');
+
+  $.get(cfg.api, function(data, status) {
+    $(el).find('.loading').remove();
+    let dt = parseIssuesData(cfg, data);
+    for (i = 0; i < cfg.group.length; i++) {
+      let lb = cfg.group[i];
+      let groupData = dt[lb];
+      if (groupData == undefined) {
+        continue;
+      }
+      if (groupData.name) {
+        $(el).append('<h2>' + groupData.name + '</h2>');
+      }
+      $(el).append('<div class="site-card-group ' + lb + '"></div>');
+      // layout items
+      for (j = 0; j < groupData.items.length; j++) {
+        let issue = groupData.items[j];
+        // get title
+        let title = issue.body.match(/title:[^\n]*\n/);
+        if (title && title.length > 0) {
+          title = title[0].replace(/(title:[\s]*|[\r\n]*)/g,'');
+        }
+        // get screenshot
+        let screenshot = issue.body.match(/screenshot:[^\n]*\n/);
+        if (screenshot && screenshot.length > 0) {
+          screenshot = screenshot[0].replace(/(screenshot:[\s]*|[\r\n]*)/g,'');
+        }
+        // get avatar
+        let avatar = issue.body.match(/avatar:[^\n]*\n/);
+        if (avatar && avatar.length > 0) {
+          avatar = avatar[0].replace(/(avatar:[\s]*|[\r\n]*)/g,'');
+        }
+        // get desc
+        let desc = issue.body.match(/description:[^\n]*\n/);
+        if (desc && desc.length > 0) {
+          desc = desc[0].replace(/(description:[\s]*|[\r\n]*)/g,'');
+        }
+
+        let imgTag = '';
+        if (screenshot && screenshot.length > 0) {
+          imgTag = '<div class="img"><img no-lazy src="' + screenshot + '" onerror="javascript:this.src=\'https://cdn.jsdelivr.net/gh/volantis-x/cdn-wallpaper-minimalist/2020/052.jpg\';"/></div>';
+        } else {
+          imgTag = '<div class="img"><img no-lazy src="https://cdn.jsdelivr.net/gh/volantis-x/cdn-wallpaper-minimalist/2020/052.jpg"/></div>';
+        }
+        let infoTag = '<div class="info">';
+        if (avatar && avatar.length > 0) {
+          infoTag += '<img no-lazy src="' + avatar + '" onerror="javascript:this.src=\'https://cdn.jsdelivr.net/gh/volantis-x/cdn-wallpaper-minimalist/2020/052.jpg\';"/>';
+        }
+        infoTag += '<span class="title">' + title + '</span><span class="desc">' + desc + '</span></div>';
+        let cardTag = "<a class='site-card' target='_blank' href='" + issue.title + "'>" + imgTag + infoTag + "</a>";
+        $(el).find('.site-card-group.' + lb).append(cardTag);
+      }
+    }
+  });
+}
+
+function getIssuesAPIForTimeline(cfg) {
+  let el = $(cfg.el)[0];
+  $(el).append('<div class="loading"><i class="fa fa-cog fa-2x fa-spin"></i><p>正在加载</p></div>');
+  $.get(cfg.api, function(data, status) {
+    $(el).find('.loading').remove();
+    if (data.length > 0) {
+      for (i = 0; i < data.length; i++) {
+        let a = '&nbsp;&nbsp;<a class="comments" target="_blank" href="' + data[i].html_url + '"><i class="fa fa-comment-dots fa-fw"></i>' + data[i].comments + '</a>';
+        let meta = '<div class="meta"><p></p><p>' + data[i].title + a + '</p><p></p></div>';
+        let body = '<div class="body"><p>' + data[i].body + '</p></div>';
+        let tag = '<div class="timenode">' + meta + body + '</div>';
+        $(el).append(tag);
+      }
+    }
+  });
+}
+
+function checkIssues() {
+  let els = document.getElementsByClassName('issues-api');
+  for (var i = 0; i < els.length; i++) {
+    let el = els[i];
+    let api = el.getAttribute('api');
+    let group = el.getAttribute('group');
+    var cfg = new Object();
+    cfg.class = el.getAttribute('class');
+    cfg.el = el;
+    cfg.api = api;
+    cfg.group = group ? group.replace(/(,[\s]*)/g,',').split(',') : undefined;
+    if (cfg.class.split(' ').includes('sites')) {
+      getIssuesAPIForSites(cfg);
+    } else if (cfg.class.split(' ').includes('timeline')) {
+      getIssuesAPIForTimeline(cfg);
+    }
+
+  }
+}
+document.addEventListener('DOMContentLoaded', function () {
+  checkIssues();
+});
+document.addEventListener('pjax:complete', function () {
+  console.log('checkIssues');
+  checkIssues();
+});
