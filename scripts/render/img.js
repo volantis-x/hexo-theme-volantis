acc995dc7888943dbc0a8f75387934d97f7a6b85
xaoxuu <xaoxuu@gmail.com>
2020-10-15 15:52:45 +0800

    image-caption

---
+++ scripts/render/img.js
@@ -0,0 +1,8 @@
+/* global hexo */
+
+'use strict';
+
+hexo.extend.filter.register('after_post_render', function(data){
+  data.content = data.content.replace(/<p><img src="(.*?)" alt="(.*?)"><\/p>/g, '<div class="img-wrap"><div class="img-bg"><img class="img" src="$1" alt="$2"\/><\/div><span class="image-caption">$2<\/span><\/div>');
+  return data;
+});
