4e39de2ecfaa4dc75afe7c65b0a55c8fc0156ef1
szyink <me@szyink.com>
2021-03-14 19:16:57 +0800

    ART-TEMPLATE
    高性能 JavaScript 模板引擎

--- source/js/issues.js
+++ source/js/issues.js
@@ -109,34 +109,32 @@ const IssuesAPI = {
       $(el).find('.loading').remove();
       const dt = IssuesAPI.groupIssuesData(cfg, data);
       const groupTitles = Object.keys(dt);
-      cfg.group.forEach((groupTitle, i) => {
-        const issues = dt[groupTitle];
-        if (issues && issues.length > 0) {
-          if (groupTitle.length > 0) {
-            $(el).append('<h2>' + groupTitle + '</h2>');
-          } else if (name == '' && groupTitles.length > 1) {
-            $(el).append('<h2>' + '未分组' + '</h2>');
-          }
-          $(el).append('<div class="site-card-group ' + i + '"></div>');
-          // layout items
-          for (j = 0; j < issues.length; j++) {
-            const issue = issues[j];
-            let imgTag = '';
-            if (issue.screenshot && issue.screenshot.length > 0) {
-              imgTag = '<div class="img"><img src="' + issue.screenshot + '" onerror="javascript:this.src=\'https://image.thum.io/get/width/1024/crop/768/' + issue.url + '\';"/></div>';
-            } else {
-              imgTag = '<div class="img"></div>';
-            }
-            let infoTag = '<div class="info">';
-            if (issue.avatar && issue.avatar.length > 0) {
-              infoTag += '<img src="' + issue.avatar + '" onerror="javascript:this.src=\'https://image.thum.io/get/width/1024/crop/768/' + issue.url + '\';"/>';
-            }
-            infoTag += '<span class="title">' + issue.title + '</span><span class="desc">' + issue.description + '</span></div>';
-            const cardTag = '<a class=\'site-card\' target=\'_blank\' href=\'' + issue.url + '\'>' + imgTag + infoTag + '</a>';
-            $(el).find('.site-card-group.' + i).append(cardTag);
-          }
-        }
-      });
+      const isuuleSitesTemp =
+        '{{each groups groupTitle}}' +
+        ' {{set issues = dt[groupTitle]}}' +
+        ' {{if issues && issues.length > 0}}' +
+        '   {{if groupTitle.length > 0}}' +
+        '     <h2>{{groupTitle}}</h2>' +
+        '   {{else if groupTitles.length > 1}}' +
+        '     <h2>未分组</h2>' +
+        '   {{/if}}' +
+        '   <div class="site-card-group">' +
+        '     {{each issues issue}}' +
+        '       {{set screenshot = "https://image.thum.io/get/width/1024/crop/768/" + issue.url }}' +
+        '       <a class="site-card" target="_blank" href="{{issue.url}}">' +
+        '         <div class="img">' +
+        '           <img src="{{issue.screenshot || screenshot}}" />' +
+        '         </div>' +
+        '         <div class="info">' +
+        '           <img src="{{issue.avatar || screenshot}}" />' +
+        '           <span class="title">{{issue.title}}</span><span class="desc">{{issue.description}}</span>' +
+        '         </div>' +
+        '       </a>' +
+        '     {{/each}}' +
+        '   </div>' +
+        ' {{/if}}' +
+        '{{/each}}';
+      $(el).append(template.render(isuuleSitesTemp, {groups: cfg.group, dt: dt, groupTitles: groupTitles}));
     }, function() {
       $(el).find('.loading i').remove();
       $(el).find('.loading p').text('加载失败，请稍后重试。');
@@ -147,15 +145,22 @@ const IssuesAPI = {
     $(el).append('<div class="loading"><i class="fa fa-cog fa-2x fa-spin"></i><p>正在加载</p></div>');
     this.requestIssuesAPI(cfg.api, function(data) {
       $(el).find('.loading').remove();
-      if (data.length > 0) {
-        for (i = 0; i < data.length; i++) {
-          const a = '&nbsp;&nbsp;<a class="comments" target="_blank" href="' + data[i].html_url + '"><i class="fa fa-comment-dots fa-fw"></i>' + data[i].comments + '</a>';
-          const meta = '<div class="meta"><p></p><p>' + data[i].title + a + '</p><p></p></div>';
-          const body = '<div class="body"><p>' + data[i].body + '</p></div>';
-          const tag = '<div class="timenode">' + meta + body + '</div>';
-          $(el).append(tag);
-        }
-      }
+      const timelineTemp =
+          '{{if data.length > 0}}' +
+          ' {{each data item}}' +
+          '   <div class="timenode">' +
+          '     <div class="meta">' +
+          '       <p></p><p>{{item.title}}&nbsp;&nbsp;' +
+          '         <a class="comments" target="_blank" href="{{item.html_url}}">' +
+          '           <i class="fa fa-comment-dots fa-fw"></i>{{item.comments}}' +
+          '         </a>' +
+          '       </p><p></p>' +
+          '     </div>' +
+          '     <div class="body"><p>{{item.body}}</p>></div>' +
+          '   </div>' +
+          ' {{/each}}' +
+          '{{/if}}';
+      $(el).append(template.render(timelineTemp, data));
     }, function() {
       $(el).find('.loading i').remove();
       $(el).find('.loading p').text('加载失败，请稍后重试。');
@@ -165,16 +170,15 @@ const IssuesAPI = {
     const els = document.getElementsByClassName('issues-api');
     for (var i = 0; i < els.length; i++) {
       const el = els[i];
-      const api = el.getAttribute('api');
-      const group = el.getAttribute('group');
-      var cfg = new Object();
-      cfg.class = el.getAttribute('class');
-      cfg.el = el;
-      cfg.api = api;
-      cfg.group = group;
-      if (cfg.class.split(' ').includes('sites')) {
+      const cfg = {
+        el: el,
+        api: el.getAttribute('api'),
+        group: el.getAttribute('group'),
+        classList: el.getAttribute('class')
+      }
+      if (cfg.classList.split(' ').includes('sites')) {
         this.getIssuesAPIForSites(cfg);
-      } else if (cfg.class.split(' ').includes('timeline')) {
+      } else if (cfg.classList.split(' ').includes('timeline')) {
         this.getIssuesAPIForTimeline(cfg);
       }
     }
