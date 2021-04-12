38c6c34fce7035749aabb6a021dca4085c5f8187
szyink <me@szyink.com>
2021-04-12 11:05:11 +0800

    #610 Gallery 支持分组显示

--- scripts/tags/fancybox.js
+++ scripts/tags/fancybox.js
@@ -4,12 +4,13 @@ function postFancybox(args, content) {
   args = args.join(' ').split(',');
   var cls = args[0];
   var col = Number(args[1]) || 0;
+  var group = (args[2] || 'default').trim();
   if (col > 0) {
-    return `<div class="gallery ${cls}" col='${col}'>
+    return `<div class="gallery ${cls}" col='${col}' data-group='${group}'>
               ${hexo.render.renderSync({text: content, engine: 'markdown'}).split('\n').join('')}
             </div>`;
   }
-  return `<div class="gallery ${cls}">
+  return `<div class="gallery ${cls}" group='${group}'>
               ${hexo.render.renderSync({text: content, engine: 'markdown'}).split('\n').join('')}
             </div>`;
 
