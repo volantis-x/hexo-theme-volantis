13dea83f438a3d4ce71b067a550d5a6f727a1f56
MHuiG <616001163@qq.com>
2021-06-28 08:21:24 +0800

    备注

--- scripts/tags/issues.js
+++ scripts/tags/issues.js
@@ -1,5 +1,9 @@
 /**
  * issues.js | https://github.com/volantis-x/hexo-theme-volantis
+ * 
+ * 备注: issues 标签在5.0版本移除 被 sites标签 friends标签 contributors标签 替代 https://github.com/volantis-x/hexo-theme-volantis/commit/abfd5eef38288f40b6a349772f84623da0b474f3
+ * 已移除的样式: https://github.com/volantis-x/hexo-theme-volantis/blob/fe81586d60053973322bd9117e2022dc119b5283/source/css/_tag-plugins/site-card.styl
+ * 已移除的脚本: https://github.com/volantis-x/hexo-theme-volantis/blob/fe81586d60053973322bd9117e2022dc119b5283/source/js/issues.js
  */
 
 'use strict';
