07f91c305d85991b837523cbd757e3e04f19ea76
MHuiG <616001163@qq.com>
2021-08-21 20:58:52 +0800

    Update seo.js

--- scripts/filters/seo.js
+++ scripts/filters/seo.js
@@ -2,19 +2,19 @@
 
 hexo.extend.filter.register('after_render:html', function (data) {
   // img 设置默认 alt https://web.dev/image-alt/
-  data = data.replace(/<img(.*?)>/gi, function (str, p) {
+  data = data.replace(/<img (.*?)>/gi, function (str, p) {
     if (p && !str.includes('alt=')) {
-      str = str.replace(p, p + ' alt="Volantis" ');
+      str = str.replace(p, p + ' alt="This is a picture without description" ');
     }
     return str
   });
   // a 设置默认 href 确保链接可供抓取  https://developers.google.com/search/docs/advanced/guidelines/links-crawlable
-  data = data.replace(/<a(.*?)>/gi, function (str, p) {
+  data = data.replace(/<a (.*?)>/gi, function (str, p) {
     if (!p) {
-      str = `<a href="/" onclick="return false;" title="Volantis">`
+      str = `<a href="/" onclick="return false;" title="Home">`
     }
     if (p && !str.includes('href=')) {
-      str = str.replace(p, p + ' href="/" onclick="return false;" title="Volantis" ');
+      str = str.replace(p, p + ' href="/" onclick="return false;" title="Home" ');
     }
     return str
   });
