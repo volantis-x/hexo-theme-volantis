3046ed18238e404d088c15d8e883387e5097ae8d
MHuiG <616001163@qq.com>
2021-08-27 16:44:16 +0800

    重构TagCSS

--- scripts/helpers/tag-plugins-style/index.js
+++ scripts/helpers/tag-plugins-style/index.js
@@ -7,6 +7,11 @@ hexo.database.model("TagPluginCSS", new Database.Schema({
   data: { type: String }
 }));
 
+hexo.database.model("TagPluginOfPost", new Database.Schema({
+  _id: { type: String, required: true },
+  tags: { type: Array }
+}));
+
 const findTags = (data) => {
   dataList = data.match(/{% (\w*?)\W.*?%}/g)
   let tagSet = new Set()
@@ -30,26 +35,55 @@ const renderStyl = (styList, callback) => {
   })
   hexo.renderStylus(tagPageStyle).then(callback)
 }
-
-// 获取标签
-hexo.extend.filter.register('before_post_render', function (data) {
-  if (!hexo.theme.config.plugins.tag_plugin_load_on_demand.enable) {
-    return data
+function getTagPluginCSSInfo() {
+  let info = hexo.locals.get('TagPluginCSSInfo')
+  if (!info) {
+    info = fs.readFileSync(
+      hexo.theme.context.theme_dir + "source/css/_style/_tag-plugins/tag.json"
+    );
+    info = JSON.parse(info)
+    hexo.locals.set('TagPluginCSSInfo', function () {
+      return info
+    });
   }
-  let uuidTagCSS = hexo.createUuid()
-  data.uuidTagCSS = uuidTagCSS
-  let tagList = findTags(data._content)
-  let info = fs.readFileSync(
-    hexo.theme.context.theme_dir + "source/css/_style/_tag-plugins/tag.json"
-  );
-  info = JSON.parse(info)
+  return info
+}
+
+function setTagPluginCSS(uuidTagCSS, css) {
+  let TagPluginCSS = hexo.locals.get('TagPluginCSS')
+  if (!TagPluginCSS) {
+    TagPluginCSS = {}
+  }
+  TagPluginCSS[uuidTagCSS] = css
+  hexo.locals.set('TagPluginCSS', function () {
+    return TagPluginCSS
+  });
+}
+function renderTagCSSAllPage(info) {
+  let styListAllPage = []
+  hexo.theme.config.plugins.tag_plugin_load_on_demand.plugins.forEach(e => {
+    if (e in info) {
+      info[e].forEach(x => {
+        styListAllPage.push(x)
+      })
+    }
+  })
+  renderStyl(styListAllPage, (css) => {
+    setTagPluginCSS("AllPage", css)
+  })
+
+}
+function insertTagCSS(uuidTagCSS, tagList) {
+  let info = getTagPluginCSSInfo()
   let styList = []
+  let tagListSave = []
   if (tagList) {
     tagList.forEach(e => {
       if (e in info) {
         info[e].forEach(x => {
           styList.push(x)
         })
+        tagListSave.push(e)
       }
     })
   }
@@ -58,45 +92,102 @@ hexo.extend.filter.register('before_post_render', function (data) {
       info[e].forEach(x => {
         styList.push(x)
       })
+      tagListSave.push(e)
     }
   })
   renderStyl(styList, (css) => {
-    hexo.database.model('TagPluginCSS').insert({
-      _id: uuidTagCSS,
-      data: css,
-    })
+    setTagPluginCSS(uuidTagCSS, css)
   })
   if (!hexo.locals.get('TagCSSAllPageFlag') && hexo.theme.config.plugins.tag_plugin_load_on_demand.plugins) {
     hexo.locals.set('TagCSSAllPageFlag', function () {
       return 1
     });
-    let styListAllPage = []
-    hexo.theme.config.plugins.tag_plugin_load_on_demand.plugins.forEach(e => {
+    renderTagCSSAllPage(info)
+  }
+  hexo.database.model('TagPluginOfPost').insert({
+    _id: uuidTagCSS,
+    tags: tagListSave,
+  })
+}
+
+function updateTagCSS(uuidTagCSS) {
+  // 这里在第二次刷新页面时生效
+  let arg = process.argv[2];
+  if (arg != "s" && arg != "server") {
+    return
+  }
+  let info = getTagPluginCSSInfo()
+  let res = hexo.database.model('TagPluginOfPost').find({ _id: uuidTagCSS })
+  if (res && res.data && res.data[0] && res.data[0].tags) {
+    let tags = res.data[0].tags;
+    let styList = []
+    tags.forEach(e => {
       if (e in info) {
         info[e].forEach(x => {
-          styListAllPage.push(x)
+          styList.push(x)
         })
       }
     })
-    renderStyl(styListAllPage, (css) => {
-      hexo.database.model('TagPluginCSS').insert({
-        _id: "AllPage",
-        data: css,
-      })
+    renderStyl(styList, (css) => {
+      setTagPluginCSS(uuidTagCSS, css)
     })
-  }
-  return data;
-});
-function getTagCSS(page) {
-  let res = hexo.database.model('TagPluginCSS').find({ _id: page.uuidTagCSS })
-  if (res && res.data && res.data[0] && res.data[0].data) {
-    return res.data[0].data
   } else {
-    let res = hexo.database.model('TagPluginCSS').find({ _id: "AllPage" })
-    if (res && res.data && res.data[0] && res.data[0].data) {
-      return res.data[0].data
-    }
+    renderTagCSSAllPage(info)
   }
 }
 
+function generateTagCSS(data) {
+  if (!hexo.theme.config.plugins.tag_plugin_load_on_demand.enable) {
+    return data
+  }
+  let uuidTagCSS = hexo.createUuid()
+  data.uuidTagCSS = uuidTagCSS
+  let tagList = findTags(data._content)
+  insertTagCSS(uuidTagCSS, tagList)
+  return data;
+}
+
+function getTagCSS(page) {
+  updateTagCSS(page.uuidTagCSS)
+  let TagPluginCSS = hexo.locals.get('TagPluginCSS')
+  if (TagPluginCSS) {
+    if (TagPluginCSS[page.uuidTagCSS]) {
+      return TagPluginCSS[page.uuidTagCSS]
+    } else {
+      return TagPluginCSS["AllPage"]
+    }
+  }
+}
+hexo.extend.filter.register("before_generate", function () {
+  let arg = process.argv[2];
+  if (arg != "s" && arg != "server") {
+    return
+  }
+  // 只执行一次
+  if (hexo.locals.get('TagCSSbeforeFlag')) {
+    return
+  }
+  hexo.locals.set('TagCSSbeforeFlag', function () {
+    return 1
+  });
+  let info = getTagPluginCSSInfo()
+  hexo.database.model('TagPluginOfPost').forEach(x => {
+    if (x.tags) {
+      let tags = x.tags;
+      let styList = []
+      tags.forEach(e => {
+        if (e in info) {
+          info[e].forEach(x => {
+            styList.push(x)
+          })
+        }
+      })
+      renderStyl(styList, (css) => {
+        setTagPluginCSS(x._id, css)
+      })
+    }
+  })
+  renderTagCSSAllPage(info)
+});
+hexo.extend.filter.register('before_post_render', generateTagCSS)
 hexo.extend.helper.register("TagCSS", getTagCSS);
