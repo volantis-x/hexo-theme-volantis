4b6de95fe55e9dea4d7a91667f93ee9414857d6c
szyink <me@szyink.com>
2021-09-01 02:50:47 +0800

    hide btn

--- source/js/app.js
+++ source/js/app.js
@@ -220,7 +220,7 @@ const VolantisApp = (() => {
         fn.scrolltoElement(volantis.dom.commentTarget);
         e.stopImmediatePropagation();
       });
-    } else volantis.dom.comment.remove(); // 关闭了评论，则隐藏评论按钮
+    } else volantis.dom.comment.style.display='none'; // 关闭了评论，则隐藏评论按钮
 
     // 移动端toc目录按钮 【移动端】
     if (volantis.isMobile) {
@@ -239,7 +239,7 @@ const VolantisApp = (() => {
           volantis.dom.tocTarget.removeClass('active');
           volantis.dom.toc.removeClass('active');
         });
-      } else volantis.dom.toc.remove(); // 隐藏toc目录按钮
+      } else volantis.dom.toc.style.display='none'; // 隐藏toc目录按钮
     }
   }
 
