57454820f8c0e6e464daec12808756892c4824ce
MHuiG <616001163@qq.com>
2021-09-01 09:58:29 +0800

    锚点定位

--- source/js/app.js
+++ source/js/app.js
@@ -30,12 +30,12 @@ const locationHash = () => {
       setTimeout(() => {
         if (window.location.hash.startsWith('#fn')) { // hexo-reference https://github.com/volantis-x/hexo-theme-volantis/issues/647
           window.scrollTo({
-            top: target.offsetTop + volantis.dom.bodyAnchor.offsetTop - volantis.dom.header.offsetHeight,
+            top: target.getBoundingClientRect().top + document.documentElement.scrollTop - volantis.dom.header.offsetHeight - 5,
             // behavior: "smooth" //平滑滚动
           });
         } else {
           window.scrollTo({
-            top: target.offsetTop + volantis.dom.bodyAnchor.offsetTop + 5,
+            top: target.getBoundingClientRect().top + document.documentElement.scrollTop + 5, // 锚点中上半部有大片空白 高度大概是 volantis.dom.header.offsetHeight
             // behavior: "smooth" //平滑滚动
           });
         }
@@ -394,7 +394,7 @@ const VolantisApp = (() => {
         let target = document.getElementById(targetID);
         if (target) {
           window.scrollTo({
-            top: target.offsetTop + volantis.dom.bodyAnchor.offsetTop - volantis.dom.header.offsetHeight,
+            top: target.getBoundingClientRect().top + document.documentElement.scrollTop - volantis.dom.header.offsetHeight - 5,
             // behavior: "smooth" //平滑滚动
           });
         }
