8d17345b9b84c11196714ae53064cf756c563046
inkss <me@szyink.com>
2021-12-18 16:46:07 +0800

    评论灯箱支持
    artalk, minivline, twikoo, valine, vssue, waline

--- source/js/app.js
+++ source/js/app.js
@@ -496,10 +496,12 @@ const volantisFancyBox = (() => {
       }
     })
 
-    Fancybox.destroy();
     for (const iterator of group) {
       if (!!iterator) Fancybox.bind('[data-fancybox="' + iterator + '"]', {
-        Hash: false
+        Hash: false,
+        Thumbs: {
+          autoStart: false,
+        }
       });
     }
   }
@@ -557,7 +559,14 @@ const volantisFancyBox = (() => {
   fn.bind = (selectors) => {
     fn.checkFancyBox(false, () => {
       Fancybox.bind(selectors, {
-        groupAll : true
+        groupAll : true,
+        Hash: false,
+        Thumbs: {
+          autoStart: false,
+        },
+        caption: function (fancybox, carousel, slide) {
+          return slide.$trigger.alt || null
+        }
       });
     });
   }
