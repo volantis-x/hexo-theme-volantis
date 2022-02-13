455a909ffd35a6f131090ecafc51469ddd6020d8
szyink <me@szyink.com>
2022-02-13 19:07:31 +0800

    测试

---
+++ scripts/generator/build-config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+hexo.extend.generator.register('volantis-init-config', () => {
+  debugger;
+  return [
+    { path: 'volantis-webconfig.json', data: hexo.config },
+    { path: 'volantis-themeconfig.json', data: hexo.theme.config }
+  ]
+});
