36e805fa6395999407191d8391e3e01a7c865af1
MHuiG <616001163@qq.com>
2022-08-15 17:26:36 +0800

    收集 CDN 资源列表

--- scripts/events/lib/cdn.js
+++ scripts/events/lib/cdn.js
@@ -1,4 +1,5 @@
 const { version } = require('../../../package.json');
+const theme_version = version;
 const path = require('path')
 const site_root = hexo.config.root;
 const cdn_info = hexo.render.renderSync({ path: path.join(hexo.theme_dir, '/_cdn.yml'), engine: 'yaml' })
@@ -51,6 +52,64 @@ function volantis_cdn_system_prefix(source, hexo) {
   }
 }
 
+const minFile = (file) => {
+  return file.replace(/(?<!\.min)\.(js|css)$/g, ext => '.min' + ext)
+}
+
+// 匹配 CDN 资源
+function match_cdn_source(key) {
+  if (key && cdn_info[key]) {
+    const info = cdn_info[key];
+    const system_config = hexo.theme.config.cdn_system.system_config;
+    for (const iterator of system_config.priority) {
+      if (iterator === "custom") {
+        if (system_config.custom) {
+          const r = system_config.custom[info.name];
+          if (r) {
+            return r;
+          }
+        }
+      } else {
+        if (info[iterator] === true) {
+          const prefix = system_config[iterator].prefix?.replace(/\$\{site_root\}/g, site_root);
+          const format = system_config[iterator].format;
+          const name = info.name;
+          let file = info.file;
+          if (iterator === "cdnjs") {
+            file = file.replace(/^[lib|dist]*\/|browser\//g, '')
+          }
+          if (info.local === true) {
+            file = file.replace(/^source\//g, '')
+          }
+          const min_file = minFile(file)
+          let version = info.version;
+          if (info.local === true) {
+            version = theme_version
+          }
+          const value = {
+            version,
+            name,
+            file,
+            min_file,
+            prefix,
+          }
+          return format.replace(/\$\{(.+?)\}/g, (match, $1) => value[$1])
+        }
+      }
+    }
+  }
+  return null;
+}
+
+// 收集 CDN 资源
+function collect_cdn_source() {
+  hexo.theme.config.cdn1 = {}
+  Object.keys(cdn_info).forEach(e => {
+    hexo.theme.config.cdn1[e] = match_cdn_source(e)
+  })
+  console.log(hexo.theme.config.cdn1);
+}
+
 hexo.on('generateBefore', () => {
   volantis_cdn_system_prefix(hexo.theme.config, hexo);
   // 可以在 source/_data/cdn.yml 覆盖 theme/_cdn.yml
@@ -58,4 +117,5 @@ hexo.on('generateBefore', () => {
   if (data.cdn) {
     merge(cdn_info, data.cdn);
   }
+  collect_cdn_source();
 });
