f82f54718b081439ea40e5a105ba81be15c60de0
MHuiG <616001163@qq.com>
2022-11-24 09:27:11 +0800

    perf(debug): description 的配置检查改为warn警告并使用默认值 #801

--- scripts/events/lib/check-configuration.js
+++ scripts/events/lib/check-configuration.js
@@ -3,17 +3,19 @@ module.exports =(hexo) => {
     let config = hexo.config;
     let themeConfig = hexo.theme.config;
     if (!config.title) {
-      return `title 未配置！
+      config.title = `Volantis`
+      hexo.log.warn(`title 未配置！
 请在站点配置 _config.yml 中配置 title
 see: https://hexo.io/zh-cn/docs/configuration
-title is not configured!`
+title is not configured!`)
     }
     if (!config.description) {
-      return `description 未配置！
+      config.description = `Volantis 是一个功能丰富、高度模块化的 Hexo 博客主题。得益于其强大的模块化特性，您可以轻松搭建一个极简风格的博客，也可以仿照官网搭建一个多人协作的、包含文档模块的大体量综合型博客。`
+      hexo.log.warn(`description 未配置！
 请在站点配置 _config.yml 中配置 description
 description主要用于SEO，告诉搜索引擎一个关于您站点的简单描述，通常建议在其中包含您网站的关键词。
 see: https://hexo.io/zh-cn/docs/configuration
-description is not configured!`
+description is not configured!`);
     }
     if (themeConfig?.search?.service===`google`||themeConfig?.search?.service===`algolia`||themeConfig?.search?.service===`azure`||themeConfig?.search?.service===`baidu`) {
       return `原 google, algolia, azure, baidu 站内搜索 系祖传代码, 且文档丢失, 不便后续维护 在 5.0 版本被移除
