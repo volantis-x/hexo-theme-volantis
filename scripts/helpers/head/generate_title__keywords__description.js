d5e5557bad310059778aaf80879e84da8c01c312
H <38281581+MHuiG@users.noreply.github.com>
2025-11-16 21:17:09 +0800

    Update generate_title__keywords__description.js

--- scripts/helpers/head/generate_title__keywords__description.js
+++ scripts/helpers/head/generate_title__keywords__description.js
@@ -77,7 +77,6 @@ hexo.extend.helper.register("generate_title", function (config, theme, page) {
   }
   s += `${config.title}`
   s = hexo.strip_html(s)
-  s = hexo.escapeHTML(s)
   return `<title>${s}</title>`
 });
 
@@ -92,7 +91,6 @@ hexo.extend.helper.register("generate_keywords", function (config, theme, page) 
     keywords += `${config.keywords}`
   }
   keywords = hexo.strip_html(keywords)
-  keywords = hexo.escapeHTML(keywords)
   return `<meta name="keywords" content="${keywords}">`
 });
 
@@ -109,7 +107,6 @@ hexo.extend.helper.register("generate_description", function (config, theme, pag
   }
   description += ` - ${config.author} - ${config.title}`
   description = hexo.strip_html(description)
-  description = hexo.escapeHTML(description)
   return `<meta desc name="description" content="${description}">`
 });
 // open_graph() 函数会生成一个 description 标签???  https://github.com/hexojs/hexo/blob/92b979f4a3fa8714aebd3d11c3295d466b870905/lib/plugins/helper/open_graph.js#L98
