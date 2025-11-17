dc403691071a07cb433de34becdb0d35e59c9afa
H <38281581+MHuiG@users.noreply.github.com>
2025-11-17 10:29:52 +0800

    fix: remove noarchive

--- scripts/helpers/head/generate_seo.js
+++ scripts/helpers/head/generate_seo.js
@@ -31,8 +31,8 @@ hexo.extend.helper.register('generate_seo', function (theme, page) {
   }
   if (robots_content) {
     robots_content = addImagePreviewIfNoNoindex(robots_content)
-    return `<meta name="robots" content="${robots_content},noarchive">`
+    return `<meta name="robots" content="${robots_content}">`
   }else{
-    return `<meta name="robots" content="index,follow,max-image-preview:large,noarchive">`
+    return `<meta name="robots" content="index,follow,max-image-preview:large">`
   }
 });
