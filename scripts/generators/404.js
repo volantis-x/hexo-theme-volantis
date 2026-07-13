70368245a5564eb6ae4c17e85827ee46406c0a6d
H <38281581+MHuiG@users.noreply.github.com>
2026-07-13 09:37:21 +0800

    Update 404.js

--- scripts/generators/404.js
+++ scripts/generators/404.js
@@ -1,9 +1,20 @@
 hexo.extend.generator.register('404', function (locals) {
-  const { site_tree } = hexo.theme.config
-  site_tree["404"].data.layout = site_tree["404"].layout;
+  const { site_tree } = hexo.theme.config;
+  const data = Object.assign({
+      layout: site_tree["404"].layout,
+      body: ["article", "comments"],
+      top_meta: false,
+      bottom_meta: false,
+      sidebar: [],
+      index: false,
+      robots: noindex,nofollow,
+      sitemap: false,
+      date: "2020-1-1 1:1",
+      content: '<p class="p logo center huge">404</p><p class="p center bold">很抱歉，您访问的页面不存在</p><p class="p center small">可能是输入地址有误或该地址已被删除</p>'
+  }, site_tree["404"].data||{});
   return {
     path: site_tree["404"].path,
     layout: [site_tree["404"].layout],
-    data: site_tree["404"].data
+    data: data
   }
 })
