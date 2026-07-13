827d46b6d503ff5f0f6fada48d1f65047e40cd83
H <38281581+MHuiG@users.noreply.github.com>
2026-07-13 10:14:56 +0800

    Update category.js

--- scripts/generators/category.js
+++ scripts/generators/category.js
@@ -1,9 +1,27 @@
 hexo.extend.generator.register('categories', function (locals) {
+  const { site_tree } = hexo.theme.config;
+  if (!site_tree.tag.enable) return;
+  const data = Object.assign({
+      layout: 'category',
+      index: true,
+      title: "Categories",
+      robots: "noindex,follow",
+      sitemap: false,
+      keywords: ["categorise"],
+      description: "categories",
+      meta: {
+        header: [],
+        footer: []
+      },
+      sidebar: [],
+      date: "2020-1-1 1:1",
+      content: ''
+  }, site_tree.tag.data||{});
   if (locals.categories && locals.categories.length > 0) {
     return {
       path: hexo.config.category_dir + '/index.html',
       layout: ['category'],
-      data: locals.posts
+      data: data
     }
   } else {
     return {}
