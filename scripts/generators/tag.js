d0ce3f4a47036916ebb474a3a0eb5c744eafaf91
H <38281581+MHuiG@users.noreply.github.com>
2026-07-13 09:56:21 +0800

    Update and rename tags.js to tag.js

--- scripts/generators/tag.js
+++ scripts/generators/tag.js
@@ -2,7 +2,7 @@ hexo.extend.generator.register('tags', function (locals) {
   if (locals.tags && locals.tags.length > 0) {
     return {
       path: hexo.config.tag_dir + '/index.html',
-      layout: ['tags'],
+      layout: ['tag'],
       data: locals.posts
     }
   } else {
