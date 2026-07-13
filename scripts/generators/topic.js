96d40947d5536c3c15709016fff0a6401e69a932
H <38281581+MHuiG@users.noreply.github.com>
2026-07-13 14:37:18 +0800

    Create topic.js

---
+++ scripts/generators/topic.js
@@ -0,0 +1,17 @@
+hexo.extend.generator.register('index_topic', function (locals) {
+  const { site_tree, topic } = hexo.theme.config
+  const topicIdList = Object.keys(topic.tree)
+  if (topicIdList.length == 0) {
+    return {}
+  }
+  var ret = []
+  ret.push({
+    path: site_tree.index_topic.base_dir + '/index.html',
+    layout: ['index_topic'],
+    data: {
+      layout: 'index_topic',
+      menu_id: site_tree.index_topic.menu_id
+    }
+  })
+  return ret
+})
