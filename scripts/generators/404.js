d20dae62770a7e7764145d0f42785a24bff5fd63
H <38281581+MHuiG@users.noreply.github.com>
2026-07-13 08:33:52 +0800

    Update 404.js

--- scripts/generators/404.js
+++ scripts/generators/404.js
@@ -1,11 +1,8 @@
 hexo.extend.generator.register('404', function (locals) {
   const { site_tree } = hexo.theme.config
   return {
-    path: site_tree.error_page['404'],
-    layout: ['404'],
-    data: {
-      layout: '404',
-      menu_id: site_tree.error_page.menu_id
-    }
+    path: site_tree.404_page.path,
+    layout: [site_tree.404_page.layout],
+    data: {}
   }
 })
