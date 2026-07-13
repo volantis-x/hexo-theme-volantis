373c44533a905e5f6e4e37c40435ad1d0e73a1e0
H <38281581+MHuiG@users.noreply.github.com>
2026-07-13 14:46:44 +0800

    Update topic.js

--- scripts/generators/topic.js
+++ scripts/generators/topic.js
@@ -10,7 +10,6 @@ hexo.extend.generator.register('index_topic', function (locals) {
     layout: ['index_topic'],
     data: {
       layout: 'index_topic',
-      menu_id: site_tree.index_topic.menu_id
     }
   })
   return ret
