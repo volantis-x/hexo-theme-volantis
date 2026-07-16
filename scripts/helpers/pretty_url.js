1fef44b11f8db746bcd20f54d02de84de93b2e83
H <38281581+MHuiG@users.noreply.github.com>
2026-07-16 19:10:48 +0800

    Update pretty_url.js

--- scripts/helpers/pretty_url.js
+++ scripts/helpers/pretty_url.js
@@ -1,5 +1,11 @@
 'use strict';
 
+// 站点 _config.yml 推荐配置：
+// pretty_urls:
+//  trailing_index: false # Set to false to remove trailing 'index.html' from permalinks
+//  trailing_html: false # Set to false to remove trailing '.html' from permalinks
+
+
 hexo.extend.helper.register('pretty_url', function (path = '') {
   if (path.startsWith('http://') || path.startsWith('https://')) {
     // 如果是绝对 URL，直接返回
@@ -8,10 +14,10 @@ hexo.extend.helper.register('pretty_url', function (path = '') {
   
   let url = this.url_for(path);
 
-  // 替换 /index.html → /
+  // 替换 /index.html → /            这是文件夹
   url = url.replace(/\/index\.html$/, '/');
 
-  // 替换 /about.html → /about
+  // 替换 /about.html → /about       这是文件
   url = url.replace(/\.html$/, '');
 
 
