e2070d63f2c732e245bfdaf5dfaf5af358e62766
H <38281581+MHuiG@users.noreply.github.com>
2026-07-22 16:19:23 +0800

    Update 404.js

--- scripts/generators/404.js
+++ scripts/generators/404.js
@@ -1,5 +1,5 @@
 hexo.extend.generator.register('404', function (locals) {
-  const i18n = hexo.i18n;
+  const __ = locals.__;
   const { site_tree } = hexo.theme.config;
   if (!site_tree["404"].enable) return;
   const data = Object.assign({
@@ -12,7 +12,7 @@ hexo.extend.generator.register('404', function (locals) {
       robots: "noindex,nofollow",
       sitemap: false,
       date: "2020-1-1 1:1",
-      content: `<p class="p logo center huge">404</p><p class="p center bold">${i18n.__("error.what")}</p><p class="p center small">${i18n.__("error.why")}</p>`
+      content: `<p class="p logo center huge">404</p><p class="p center bold">${__("error.what")}</p><p class="p center small">${__("error.why")}</p>`
   }, site_tree["404"].data||{});
   return {
     path: site_tree["404"].path,
