1ece934c142b284744fe074e3994ee021900b0a8
H <38281581+MHuiG@users.noreply.github.com>
2026-07-22 16:03:32 +0800

    Update 404.js

--- scripts/generators/404.js
+++ scripts/generators/404.js
@@ -11,7 +11,7 @@ hexo.extend.generator.register('404', function (locals) {
       robots: "noindex,nofollow",
       sitemap: false,
       date: "2020-1-1 1:1",
-      content: `<p class="p logo center huge">404</p><p class="p center bold">${hexo.__("error.what")}</p><p class="p center small">${hexo.__("error.why")}</p>`
+      content: `<p class="p logo center huge">404</p><p class="p center bold">${__("error.what")}</p><p class="p center small">${__("error.why")}</p>`
   }, site_tree["404"].data||{});
   return {
     path: site_tree["404"].path,
