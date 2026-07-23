972a26fbb58b28bef32ac63673cb65824bf5408b
H <38281581+MHuiG@users.noreply.github.com>
2026-07-23 10:54:04 +0800

    Update tag-utils.js

--- scripts/events/lib/tag-utils.js
+++ scripts/events/lib/tag-utils.js
@@ -126,9 +126,9 @@ module.exports = hexo => {
       return hexo.args.map_stellar_v1(args, keys, others);
     }
     if (/::/g.test(args)) {
-      return [hexo.args.map_volantis_v5(args), "v5"];
+      return {"args": hexo.args.map_volantis_v5(args), "version" : "v5"};
     } else if (/,/g.test(args)) {
-      return [hexo.args.map_volantis_v4(args), "v5"]; // 实现上 v4 同 v5 一样
+      return {"args": hexo.args.map_volantis_v4(args), "version" : "v5"}; // 实现上 v4 同 v5 一样
     } else {
       return hexo.args.map_stellar_v1(args, keys, others);
     }
