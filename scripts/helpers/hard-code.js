3fc9e9dd0348deeeec613aca2f58d65c00db50fb
H <38281581+MHuiG@users.noreply.github.com>
2026-07-23 08:34:08 +0800

    Update hard-code.js

--- scripts/helpers/hard-code.js
+++ scripts/helpers/hard-code.js
@@ -19,3 +19,22 @@ function getFirstCSS() {
 }
 hexo.extend.filter.register("before_generate", generateFirstCSS);
 hexo.extend.helper.register("FirstCSS", getFirstCSS);
+
+
+// 将 global.js 硬编码到页面
+
+function generateGlobalJS() {
+  let s = fs.readFileSync(
+    hexo.theme.context.theme_dir + "source/css/first.styl"
+  );
+  hexo.renderStylus(s).then((css) => {
+    hexo.locals.set('FirstCSS', function () {
+      return css
+    });
+  })
+}
+function getFirstCSS() {
+  return hexo.locals.get('FirstCSS');
+}
+hexo.extend.filter.register("before_generate", generateFirstCSS);
+hexo.extend.helper.register("FirstCSS", getFirstCSS);
