3aff020192613f4bff030610d470246de488231e
H <38281581+MHuiG@users.noreply.github.com>
2026-07-24 10:17:21 +0800

    Update tabs.js

--- scripts/tags/tabs.js
+++ scripts/tags/tabs.js
@@ -1,8 +1,111 @@
 /**
+ * tabs.js v2 | 基于NexT修改： https://theme-next.js.org/docs/tag-plugins/tabs
+ */
+// v6
+/*
+{% tabs active:2 align:center %}
+
+<!-- tab 图片 -->
+{% image https://unpkg.com/volantis-static@0.0.1761982841160/media/wallpaper/minimalist/2020/001.webp %}
+
+<!-- tab 代码块 -->
+
+```swift
+let x = 123
+print("hello world")
+```
+
+<!-- tab 表格 -->
+| a | b | c |
+| --- | --- | --- |
+| a1 | b1 | c1 |
+| a2 | b2 | c2 |
+
+{% endtabs %}
+
+支持设置 align:center 来使内容居中
+设置默认激活的标签方式为 `active:1` 而非 `, 1`（使用默认格式降低学习成本，且显式声明可读性更强）
+不需要 <!-- endtab --> 来作为结束标识（因为会自动判断）
+不需要 tabs id 来保证唯一性（因为会设置唯一标识）
+不支持 @icon 方式设置图标（因为不再内置 fontawesome 图标库）
+轮廓样式简化，可以搭配其它容器类标签嵌套使用。
+*/
+
+'use strict'
+
+var tab_index = 0 
+
+function tabs_v6(args, content = '') {
+  const ctx = hexo;
+  var arr = content.split(/<!--\s*tab (.*?)\s*-->/g).filter(item => item.trim().length > 0)
+  if (arr.length < 1) {
+    return ''
+  }
+  var tabs = []
+  arr.forEach((item, i) => {
+    if (i % 2 == 0) {
+      tabs.push({
+        header: item
+      })
+    } else if (tabs.length > 0) {
+      var tab = tabs[tabs.length-1]
+      if (tab.body == undefined) {
+        tab.body = item
+      } else {
+        tab.body += '\n' + item
+      }
+    }
+  })
+
+  args = ctx.args.map(args, ['active', 'align'])
+  const tabName = 'tab_' + ++tab_index
+  const tabActive = Number(args.active) || 0
+
+  let tabId = 0
+  let tabNav = ''
+  let tabContent = ''
+  tabs.forEach((tab, i) => {
+    let content = ctx.render.renderSync({ text: (tab.body || ''), engine: 'markdown' }).trim()
+    const abbr = tabName + ' ' + ++tabId
+    const href = abbr.toLowerCase().split(' ').join('-')
+    const isActive = (tabActive > 0 && tabActive === tabId) || (tabActive === 0 && tabId === 1) ? ' active' : ''
+    tabNav += `<div class="tab${isActive}"><a href="#${href}">${tab.header || abbr}</a></div>`
+    tabContent += `<div class="tab-pane${isActive}" id="${href}">${content}</div>`
+  })
+
+  tabNav = `<div class="nav-tabs">${tabNav}</div>`
+  tabContent = `<div class="tab-content">${tabContent}</div>`
+
+  var el = ''
+  el += '<div class="tag-plugin tabs"'
+  if (args.align != undefined) {
+    el += ' align="' + args.align + '"'
+  }
+  el += 'id="' + tabName.toLowerCase().split(' ').join('-') + '"'
+  el += '>'
+  el += tabNav + tabContent
+  el += '</div>'
+  return el
+}
+
+hexo.extend.tag.register('tabs6', tabs_v6, true)
+
+
+
+/**
  * tabs.js | https://theme-next.org/docs/tag-plugins/tabs
  */
-
-'use strict';
+// v5
+/*
+{% tabs 页面内不重复的ID %}
+<!-- tab 栏目1 -->
+内容
+<!-- endtab -->
+<!-- tab 栏目2 -->
+内容
+<!-- endtab -->
+{% endtabs %}
+*/
 
 function postTabs(args, content) {
   var tabBlock = /<!--\s*tab (.*?)\s*-->\n([\w\W\s\S]*?)<!--\s*endtab\s*-->/g;
