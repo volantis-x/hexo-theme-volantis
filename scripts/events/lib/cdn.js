ec1fba39054677bb525ead989872dbb782ee3a98
H <38281581+MHuiG@users.noreply.github.com>
2026-07-26 10:44:28 +0800

    Update cdn.js

--- scripts/events/lib/cdn.js
+++ scripts/events/lib/cdn.js
@@ -1,3 +1,4 @@
+module.exports = hexo => {
 /* Getting the version of the theme. */
 const { version } = require('../../../package.json');
 const theme_version = version;
@@ -193,7 +194,6 @@ function collect_cdn_source() {
     console.log(hexo.theme.config.cdn);
 }
 
-module.exports = hexo => {
   /* It's replacing the prefix of the source with the prefix you set in the configuration file. */
   volantis_cdn_system_prefix(hexo.theme.config, hexo);
   // 可以在 source/_data/cdn.yml 覆盖 theme/_data/cdn.yml
