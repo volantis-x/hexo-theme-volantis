27952fd24f1c9aab597ad8737f9f66272585e9fa
H <38281581+MHuiG@users.noreply.github.com>
2026-07-26 08:58:11 +0800

    Update cdn.js

--- scripts/events/lib/cdn.js
+++ scripts/events/lib/cdn.js
@@ -3,9 +3,9 @@ const { version } = require('../../../package.json');
 const theme_version = version;
 const path = require('path')
 const site_root = hexo.config.root;
-// _cdn.yml
-/* It's reading the `_cdn.yml` file in the theme directory. */
-const cdn_info = hexo.render.renderSync({ path: path.join(hexo.theme_dir, '/_cdn.yml'), engine: 'yaml' })
+// _data/cdn.yml
+/* It's reading the `_data/cdn.yml` file in the theme directory. */
+const cdn_info = hexo.render.renderSync({ path: path.join(hexo.theme_dir, '/_data/cdn.yml'), engine: 'yaml' })
 
 /**
  * If the item is an object and not an array, then it's an object.
@@ -196,7 +196,7 @@ function collect_cdn_source() {
 hexo.on('generateBefore', () => {
   /* It's replacing the prefix of the source with the prefix you set in the configuration file. */
   volantis_cdn_system_prefix(hexo.theme.config, hexo);
-  // 可以在 source/_data/cdn.yml 覆盖 theme/_cdn.yml
+  // 可以在 source/_data/cdn.yml 覆盖 theme/_data/cdn.yml
   const data = hexo.locals.get('data');
   if (data.cdn) {
     merge(cdn_info, data.cdn);
