d312b62c134bfac18e570586f3c1d8cdb64de4c3
H <38281581+MHuiG@users.noreply.github.com>
2026-07-26 14:22:37 +0800

    Update tag-utils.js

--- scripts/events/lib/tag-utils.js
+++ scripts/events/lib/tag-utils.js
@@ -123,7 +123,7 @@ module.exports = hexo => {
     }
   };
   hexo.args.map = hexo.args.map_stellar_v1;
-  // volantis v6
+  // volantis v6 标签名称如果冲突，开头首字母大写，依靠参数判断版本并不现实。
  /* hexo.args.map_fix = (args, keys, others) => {
     // 若 args 不是数组，直接返回原值。
     if (Array.isArray(args) == false) {
