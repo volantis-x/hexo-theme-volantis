c0979e3a33d22875dbfa36dec8b6ec3df8bf1dd2
H <38281581+MHuiG@users.noreply.github.com>
2026-07-26 14:09:21 +0800

    Update checkbox.js

--- scripts/tags/checkbox.js
+++ scripts/tags/checkbox.js
@@ -1,3 +1,33 @@
+/**
+ * 上次修改更新：5.0 | https://github.com/volantis-x/hexo-theme-volantis
+ *
+ */
+
+/*
+{% checkbox 样式参数（可选）::文本（支持简单md） %}
+
+{% checkbox 纯文本测试 %}
+{% checkbox checked::支持简单的 [markdown](https://guides.github.com/features/mastering-markdown/) 语法 %}
+{% checkbox red::支持自定义颜色 %}
+{% checkbox green checked::绿色 + 默认选中 %}
+{% checkbox yellow checked::黄色 + 默认选中 %}
+{% checkbox cyan checked::青色 + 默认选中 %}
+{% checkbox blue checked::蓝色 + 默认选中 %}
+{% checkbox plus green checked::增加 %}
+{% checkbox minus yellow checked::减少 %}
+{% checkbox times red checked::叉 %}
+
+{% radio 样式参数（可选）::文本（支持简单md） %}
+{% radio 纯文本测试 %}
+{% radio checked::支持简单的 [markdown](https://guides.github.com/features/mastering-markdown/) 语法 %}
+{% radio red::支持自定义颜色 %}
+{% radio green::绿色 %}
+{% radio yellow::黄色 %}
+{% radio cyan::青色 %}
+{% radio blue::蓝色 %}
+
+*/
+
 'use strict';
 
 function postCheckbox(args) {
