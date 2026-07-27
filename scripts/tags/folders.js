998366172fdae2e8dfcacae49808c1289030fcfc
H <38281581+MHuiG@users.noreply.github.com>
2026-07-28 07:30:45 +0800

    Update folders.js

--- scripts/tags/folders.js
+++ scripts/tags/folders.js
@@ -1,4 +1,9 @@
 /**
+ * 上次修改更新：6.7.0 | https://github.com/volantis-x/hexo-theme-volantis
+ *
+ */
+
+/**
  * folders.js v1 | https://github.com/xaoxuu/hexo-theme-stellar/
  * 格式与官方标签插件一致使用空格分隔，中括号内的是可选参数（中括号不需要写出来）
  *
