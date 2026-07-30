9d6970ee509e5206ccba7350e7e2f57f97008aaf
H <38281581+MHuiG@users.noreply.github.com>
2026-07-30 14:28:07 +0800

    Update link.js

--- scripts/tags/link.js
+++ scripts/tags/link.js
@@ -1,5 +1,100 @@
-'use strict';
+/**
+ * 上次修改更新：6.7.0 | https://github.com/volantis-x/hexo-theme-volantis
+ *
+ */
+/**
+ * link.js v1.1 | https://github.com/xaoxuu/hexo-theme-stellar/
+ * 格式与官方标签插件一致使用空格分隔，中括号内的是可选参数（中括号不需要写出来）
+ *
+ * {% Link url [title] [desc:true/false] [icon:src] %}
+ */
 
+'use strict'
+
+function Link(args) {
+  const ctx = hexo;
+  const full_url_for = require('hexo-util').full_url_for.bind(ctx)
+  args = ctx.args.map(args, ['icon', 'desc'], ['url', 'title'])
+  if (args.url == null) {
+    return '';
+  }
+  const url = full_url_for(args.url)
+  args.api = ctx.theme.config.data_services.siteinfo?.api
+  if (args.api) {
+    args.api = args.api.replace('{href}', url)
+  }
+  var autofill = []
+  if (!args.title) {
+    autofill.push('title')
+  }
+  if (!args.icon) {
+    autofill.push('icon')
+  }
+  if (args.desc !== 'true' && args.desc !== true && !args.desc) {
+    autofill.push('desc')
+  }
+  var el = ''
+  el += '<div class="tag-plugin link dis-select">'
+  el += '<a class="link-card' + (args.desc ? ' rich' : ' plain') + '" title="' + (args.title || '') + '" href="' + args.url + '"'
+  if (args.url.includes('://')) {
+    el += ' target="_blank" rel="external nofollow noopener noreferrer"'
+  }
+  el += ' cardlink'
+  if (args.api) {
+    el += ` data-api="${args.api}"`
+  }
+  el += ' autofill="'
+  el += autofill.join(',')
+  el += '"'
+  el += '>'
+
+  function loadIcon() {
+    var el = ''
+    el += '<div class="lazy img" data-bg="' + (args.icon || ctx.theme.config.default.link) + '"></div>'
+    return el
+  }
+  function loadTitle() {
+    return '<span class="title">' + (args.title || args.url) + '</span>'
+  }
+  function loadDesc() {
+    return `<span class="cap desc footnote">${args.desc}</span>`
+  }
+  function loadLink() {
+    return '<span class="cap link footnote">' + full_url_for(args.url) + '</span>'
+  }
+
+  if (args.desc) {
+    // top
+    el += '<div class="top">'
+    el += loadIcon() + loadLink()
+    el += '</div>'
+    // bottom
+    el += '<div class="bottom">'
+    el += loadTitle() + loadDesc()
+    el += '</div>'
+  } else {
+    // left
+    el += '<div class="left">'
+    el += loadTitle() + loadLink()
+    el += '</div>'
+    // right
+    el += '<div class="right">'
+    el += loadIcon()
+    el += '</div>'
+  }
+
+  // end
+  el += '</a></div>'
+
+  return el
+}
+
+
+hexo.extend.tag.register('Link', Link)
+
+
+
+// v5
 // {% link title, url %}
 // {% link title, url, img %}
 hexo.extend.tag.register('link', function (args) {
