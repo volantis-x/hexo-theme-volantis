c18da50438b3db775b40b22231d5a9bb49b4ab9b
H <38281581+MHuiG@users.noreply.github.com>
2026-07-30 16:40:15 +0800

    Update checkbox.js

--- scripts/tags/checkbox.js
+++ scripts/tags/checkbox.js
@@ -1,8 +1,65 @@
 /**
- * 上次修改更新：5.0 | https://github.com/volantis-x/hexo-theme-volantis
+ * 上次修改更新：6.7.0 | https://github.com/volantis-x/hexo-theme-volantis
  *
  */
 
+/**
+ * checkbox.js v1 | https://github.com/xaoxuu/hexo-theme-stellar/
+ * radio.js v1 | https://github.com/xaoxuu/hexo-theme-stellar/
+ * 格式与官方标签插件一致使用空格分隔，中括号内的是可选参数（中括号不需要写出来）
+ *
+ * {% Checkbox [checked:false] [color:cyan] [symbol:plus/minus/times] text %}
+ * {% Radio [checked:false] [color:cyan] text %}
+ */
+
+'use strict'
+
+function Checkbox(args) {
+  const ctx = hexo;
+  args = ctx.args.map(args, ['color', 'checked', 'symbol'], ['text'])
+  var el = ''
+  // div
+  el += '<div class="tag-plugin colorful checkbox"'
+  el += ' ' + ctx.args.joinTags(args, ['color', 'symbol']).join(' ')
+  el += '>'
+  // input
+  el += '<input type="' + 'checkbox' + '"'
+  if (args.checked == 'true') {
+    el += ' checked="true"'
+  }
+  el += '/>'
+  // text
+  el += '<span>' + args.text + '</span>'
+  // div
+  el += '</div>'
+  return el
+}
+function Radio(args) {
+  const ctx = hexo;
+  args = ctx.args.map(args, ['color', 'checked', 'symbol'], ['text'])
+  var el = ''
+  // div
+  el += '<div class="tag-plugin colorful checkbox"'
+  el += ' ' + ctx.args.joinTags(args, ['color', 'symbol']).join(' ')
+  el += '>'
+  // input
+  el += '<input type="' + 'radio' + '"'
+  if (args.checked == 'true') {
+    el += ' checked="true"'
+  }
+  el += '/>'
+  // text
+  el += '<span>' + args.text + '</span>'
+  // div
+  el += '</div>'
+  return el
+}
+hexo.extend.tag.register('Checkbox', Checkbox)
+hexo.extend.tag.register('Radio', Radio)
+
+
+
+// v5
 /*
 {% checkbox 样式参数（可选）::文本（支持简单md） %}
 
@@ -28,7 +85,6 @@
 
 */
 
-'use strict';
 
 function postCheckbox(args) {
   if (/::/g.test(args)) {
