c9400f2149c7680bba084cbb931be59ca498e1eb
H <38281581+MHuiG@users.noreply.github.com>
2026-07-30 17:29:25 +0800

    Update video.js

--- scripts/tags/video.js
+++ scripts/tags/video.js
@@ -1,7 +1,54 @@
+/**
+ * 上次修改更新：6.7.0 | https://github.com/volantis-x/hexo-theme-volantis
+ *
+ */
+/**
+ * video.js v1.0 | https://github.com/xaoxuu/hexo-theme-stellar/
+ * 格式与官方标签插件一致使用空格分隔，中括号内的是可选参数（中括号不需要写出来）
+ *
+ * {% Video src %}
+ *
+ */
+
 'use strict';
 
+function Video(args) {
+  const ctx = hexo;
+  args = ctx.args.map(args, ['type', 'bilibili', 'youtube', 'ratio', 'width', 'autoplay'], ['src'])
+  if (args.width == null) {
+    args.width = '100%'
+  }
+  if (args.bilibili) {
+    return `<div class="tag-plugin video-player" style="aspect-ratio:${args.ratio || 16 / 9};max-width:${args.width};">
+    <iframe src="https://player.bilibili.com/player.html?bvid=${args.bilibili}&autoplay=${args.autoplay || 'false'}" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true">
+    </iframe>
+    </div>
+    `
+  }
+  if (args.youtube) {
+    if(args.autoplay == 'true' || args.autoplay == '1') { 
+      args.autoplay = '1&mute=1'
+    } else {
+      args.autoplay = '0'
+    }
+    return `<div class="tag-plugin video-player" style="aspect-ratio:${args.ratio || 16 / 9};max-width:${args.width};">
+    <iframe style="border:none" src="https://www.youtube.com/embed/${args.youtube}?rel=0&disablekb=1&playsinline=1&autoplay=${args.autoplay}" picture-in-picture="true" allowfullscreen="true" >
+    </iframe>
+    </div>
+    `
+  }
+  return `<div class="tag-plugin video-player" style="max-width:${args.width};">
+  <video controls preload playsinline webkit-playsinline>
+  <source src="${args.src}" type="${args.type || 'video/mp4'}">Your browser does not support the video tag.
+  </video>
+  </div>
+  `
+}
+hexo.extend.tag.register('Video', Video)
 
 
+
+// v5
 function postVideo(args) {
   const { config } = hexo;
   const src = args[0].trim();
