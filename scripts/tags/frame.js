fb99706b7f1b59a00bc36ecf081bdd86891714f7
H <38281581+MHuiG@users.noreply.github.com>
2026-07-30 17:05:29 +0800

    Update frame.js

--- scripts/tags/frame.js
+++ scripts/tags/frame.js
@@ -1,8 +1,83 @@
 /**
- * frame.js | https://github.com/volantis-x/hexo-theme-volantis
+ * 上次修改更新：6.7.0 | https://github.com/volantis-x/hexo-theme-volantis
+ *
+ */
+/**
+ * frame.js v1 | https://github.com/xaoxuu/hexo-theme-stellar/
+ * 格式与官方标签插件一致使用空格分隔，中括号内的是可选参数（中括号不需要写出来）
+ *
+ * {% Frame iphone11 [img:src] [video:url] [focus:top/bottom] [alt] %}
  */
 
-'use strict';
+'use strict'
+
+function Frame(args) {
+  const ctx = hexo;
+  args = ctx.args.map(args, ['focus', 'img', 'video'], ['device', 'alt'])
+  const img = args.img || ''
+  const video = args.video || ''
+  const device = args.device || ''
+  const focus = args.focus || ''
+  const alt = args.alt || ''
+  if ((img.length == 0 && video.length == 0) || device.length == 0) {
+    return
+  }
+  var el = ''
+  function imgTag(url, alt) {
+    let i = ''
+    i += '<img class="img" src="' + url + '"'
+    if (alt.length > 0) {
+      i += ' alt="' + alt + '"'
+    }
+    i += '/>'
+    return i
+  }
+  if (video.length > 0) {
+    el += '<div class="tag-plugin video-wrap">'
+    el += '<div class="frame-wrap" id="' + device + '"'
+    if (focus.length > 0) {
+      el += 'focus="' + focus + '">'
+    } else {
+      el += '>'
+    }
+    el += '<video'
+    if (img.length > 0) {
+      el += ' poster="' + img + '"'
+    }
+    el += ' playsinline="" muted="" loop="" autoplay="" preload="metadata">'
+    el += '<source src="' + video + '" type="video/mp4">'
+    el += '</video>'
+
+    el += '<div class="frame"></div>'
+    el += '</div>'
+    el += '</div>'
+  } else if (img.length > 0) {
+    el += '<div class="tag-plugin img-wrap">'
+    el += '<div class="frame-wrap" id="' + device + '"'
+    if (focus.length > 0) {
+      el += 'focus="' + focus + '">'
+    } else {
+      el += '>'
+    }
+    el += imgTag(img, alt)
+    el += '<div class="frame"></div>'
+    el += '</div>'
+    if (alt.length > 0) {
+      el += '<span class="image-caption">' + alt + '</span>'
+    }
+    el += '</div>'
+  }
+  return el
+}
+
+hexo.extend.tag.register('Frame', Frame)
+
+
+
+// v5
+/**
+ * frame.js | https://github.com/volantis-x/hexo-theme-volantis
+ */
 
 // frame 带有设备模型的图片或视频
 // {% frame device | img=xxx %}
