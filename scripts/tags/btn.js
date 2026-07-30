00f05ee3b693f5e2be2b51ca5cb7f10d3a8e4ef7
H <38281581+MHuiG@users.noreply.github.com>
2026-07-30 15:02:54 +0800

    Update btn.js

--- scripts/tags/btn.js
+++ scripts/tags/btn.js
@@ -1,54 +1,7 @@
 /**
- * 上次修改更新：6.7.0 | https://github.com/volantis-x/hexo-theme-volantis
+ * 上次修改更新：5.0 | https://github.com/volantis-x/hexo-theme-volantis
  *
  */
-
-/**
- * button.js v1.0 | https://github.com/xaoxuu/hexo-theme-stellar/
- * 格式与官方标签插件一致使用空格分隔，中括号内的是可选参数（中括号不需要写出来）
- *
- * {% button [color:color] text url [icon:key/src] [size:xs] %}
- *
- */
-
-'use strict'
-
-function button(args) {
-  const ctx = hexo;
-  args = ctx.args.map(args, ['color', 'icon', 'size'], ['text', 'url'])
-  if (!args.text) {
-    return ''
-  }
-  if (!args.url) {
-    return ''
-  }
-  if (args.url.includes(' ')) {
-    var arr = args.url.split(' ')
-    args.url = arr.pop()
-    args.text = `${args.text} ${arr.join(' ')}`
-  }
-  if (args.color == null) {
-    args.color = ctx.theme.config.tag_plugins.button.default_color
-  }
-  var el = ''
-  el += '<a class="tag-plugin colorful button"'
-  el += ' ' + ctx.args.joinTags(args, ['color', 'size']).join(' ')
-  el += ` title="${args.text}"`
-  el += ` href="${args.url}"`
-  el += '>'
-  if (args.icon) {
-    el += ctx.utils.icon(args.icon)
-  }
-  el += `<span>${args.text}</span>`
-  el += '</a>'
-  return el
-}
-
-hexo.extend.tag.register('button', button)
-
-
-
-// v5
 /*
 {% btn 样式参数（可选）::标题::链接::图标（可选） %}
 
